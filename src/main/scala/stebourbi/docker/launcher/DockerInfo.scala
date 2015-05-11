a2f6c2875e0cd28e102c3c83b6cec1b602c8d961
Stéphane Jeandeaux <stephane.jeandeaux@gmail.com>
2015-05-11 13:50:53 +0200

    TODO on windows

--- src/main/scala/stebourbi/docker/launcher/DockerInfo.scala
+++ src/main/scala/stebourbi/docker/launcher/DockerInfo.scala
@@ -17,25 +17,29 @@ object DockerInfo {
   case class DockerInfoBoot2Docker(val boot2dockerip: String) extends DockerInfo(boot2dockerip, boot2dockerip)
 
 
+
   def createDockerInfo(): DockerInfo = {
     import scala.sys.process._
-    val hasBoot2docker = {
-      System.getProperty("os.name") match {
-        case u: String if u.startsWith("Windows") => 0
-        case _ =>  "which boot2docker" #> file("/dev/null") ! new FileProcessLogger(new File("/dev/null"))
+
+    System.getProperty("os.name") match {
+      case u: String if u.startsWith("Windows") => new DockerInfoBoot2Docker("TODO")
+      case _ => {
+        val hasBoot2docker = "which boot2docker" #> file("/dev/null") ! new FileProcessLogger(new File("/dev/null"))
+        //with boot2docker
+        if (hasBoot2docker == 0) {
+          val boot2DockerIp = Process("boot2docker ip").lines.head
+          val dockerInfoValue = DockerInfoBoot2Docker(boot2DockerIp)
+          dockerInfoValue
+        } else {
+          val info = "ifconfig" #| "grep -A 1 docker0" #| "tail -1" #|
+            Seq("sed", "-e", "s/[ ]*inet \\(a[d]\\{1,2\\}r:\\)\\{0,1\\}\\([0-9.]*\\) .*/\\2/")
+          DockerInfoLinux(info.lines.head)
+        }
       }
+
     }
 
-    //with boot2docker
-    if (hasBoot2docker == 0) {
-      val boot2DockerIp = Process("boot2docker ip").lines.head
-      val dockerInfoValue = DockerInfoBoot2Docker(boot2DockerIp)
-      dockerInfoValue
-    } else {
-      val info = "ifconfig" #| "grep -A 1 docker0" #| "tail -1" #|
-        Seq("sed", "-e", "s/[ ]*inet \\(a[d]\\{1,2\\}r:\\)\\{0,1\\}\\([0-9.]*\\) .*/\\2/")
-      DockerInfoLinux(info.lines.head)
-    }
+
   }
 
   lazy val dockerInfo = createDockerInfo()
